99a13d9e7e4f1f1c8a10b00d9e7c5f7f696757b1
Jackie Gan <jackiegan123@outlook.com>
2024-06-27 00:13:18 -0400

    initial commit

---
+++ ruby/basic/IO.rb
@@ -0,0 +1,13 @@
+str =  "Hello Ruby"
+putc str 
+puts str
+print str
+
+puts "what is your age: "
+val = gets
+puts val
+
+
+# print, is a regular print wihtout newline (\n)
+# puts,  print with a new line
+# p, mainly used to debugging, raw person of the object with \n displayed
---
+++ ruby/basic/hello.rb
@@ -0,0 +1,2 @@
+puts 'hello using puts'
+print "hello world"
